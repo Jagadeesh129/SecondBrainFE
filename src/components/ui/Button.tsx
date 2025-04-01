cc68278fe331d007501f9878d48efd8ed53cb049
Mamilla Jagadeesh <mamilla.jagadeesh@soti.net>
2025-04-02 01:35:55 +0530

    Added button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,18 +3,28 @@
 
 import { ReactElement } from "react"
 
+type Variants = "primary" | "secondary";
+
 interface ButtonProps {
-    variant: "primary" | "secondary";
-    size: "sm" | "md" | "lg";
+    variant: Variants;
+    size?: "sm" | "md" | "lg";
     text: String;
     startIcon?: ReactElement;
     endIcon?: ReactElement;
-    onClick: () => void;
+    onClick?: () => void;
 }
+
+const variantStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-300 text-purple-600",
+}
+
+const defaultStyles = " rounded-md px-6 py-2 flex ";
+
 
 export const Button = (props: ButtonProps)=> {
     
-    return <button>
-
+    return <button className={variantStyles[props.variant] + defaultStyles}>
+        {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null} {props.text} {props.endIcon}
     </button>
 }
